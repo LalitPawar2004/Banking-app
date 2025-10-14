623b6270d14e1d4048103bb171b000d6a96152ab
LalitPawar2004 <lalitpawar2004@gmail.com>
2025-10-14 13:16:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Sample data
 const transactions = [
+  { amount: 40, type: 'debit', date: '14 October', description: 'ATM Withdrawal' },
   { amount: 18, type: 'debit', date: '12 October', description: 'Loan' },
   { amount: 35, type: 'debit', date: '12 October', description: 'Loan' },
   { amount: 48, type: 'debit', date: '12 October', description: 'Loan' },
